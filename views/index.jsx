5192535645511cea6ff24e42ca24b99c8bc92613
Tanmoy Mitra <tanmoymitra@gmail.com>
2017-06-21 13:08:02 +0530

    Update

--- views/index.jsx
+++ views/index.jsx
@@ -7,39 +7,39 @@ class Index extends React.Component {
       <Layout title={this.props.title}>
         <h1>{this.props.title}</h1>
         <p>This is a simple example of React with Express, how to get the browser's Geo Location</p>
-        <table class="tg">
+        <table>
           <tbody>
           <tr>
-            <td class="tg-yw4l"><strong>Public IP</strong></td>
-            <td class="tg-yw4l">{this.props.ip}</td>
+            <td>Public IP</td>
+            <td>{this.props.ip}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">Country</td>
-            <td class="tg-yw4l">{this.props.country}</td>
+            <td>Country</td>
+            <td>{this.props.country}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">Country Code</td>
-            <td class="tg-yw4l">{this.props.countryCode}</td>
+            <td>Country Code</td>
+            <td>{this.props.countryCode}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">Latitude</td>
-            <td class="tg-yw4l">{this.props.lat}</td>
+            <td>Latitude</td>
+            <td>{this.props.lat}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">Longitude</td>
-            <td class="tg-yw4l">{this.props.lng}</td>
+            <td>Longitude</td>
+            <td>{this.props.lng}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">State</td>
-            <td class="tg-yw4l">{this.props.state}</td>
+            <td>State</td>
+            <td>{this.props.state}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">City</td>
-            <td class="tg-yw4l">{this.props.city}</td>
+            <td>City</td>
+            <td>{this.props.city}</td>
           </tr>
           <tr>
-            <td class="tg-yw4l">Zip Code</td>
-            <td class="tg-yw4l">{this.props.zip}</td>
+            <td>Zip Code</td>
+            <td>{this.props.zip}</td>
           </tr>
           </tbody>
         </table>
